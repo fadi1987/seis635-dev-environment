12710c1fe795b2e2aa0ab94c56e4831b8b902eef
Esten Rye <esten.rye@ryezone.com>
2020-02-25 02:07:11 -0600

    Install Maven.

--- config/provision.sh
+++ config/provision.sh
@@ -5,5 +5,6 @@ sudo apt-get install -y ansible
 cp /home/vagrant/config/ansible.cfg /home/vagrant/.ansible.cfg
 chown vagrant /home/vagrant/.ansible.cfg
 chgrp vagrant /home/vagrant/.ansible.cfg
+ansible-galaxy install -r /home/vagrant/config/requirements.yml
 ZSH='/home/vagrant/.oh-my-zsh' ansible-playbook /home/vagrant/config/java-workstation.yml
 echo "vagrant:vagrant" | chpasswd 
